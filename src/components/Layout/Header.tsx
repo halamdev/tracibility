9a3fafd9078a17ffdda8f9f70dd9c9613f62ef8c
tienlamict <tienlamict@gmail.com>
2025-07-21 23:38:30 +0700

    fix wallet

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -24,6 +24,7 @@ export const Header: React.FC = () => {
 
   const handleLogout = () => {
     localStorage.removeItem("token");
+    disconnectWallet();
     setIsLoggedIn(false);
     navigate("/");
   };
